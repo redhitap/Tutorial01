8941fdb125633c1539d5dd38a22bea22f1e91ea5
redhita.putri <redhita.putri>
2018-09-07 14:27:38 +0700

    initial commit mahasiswa.java

---
+++ Mahasiswa.java
@@ -0,0 +1,26 @@
+public class Mahasiswa {
+	public String nama;
+	public String nmp;
+	public String jurusan;
+	public String getNama() {
+		return nama;
+	}
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+	public String getNmp() {
+		return nmp;
+	}
+	public void setNmp(String nmp) {
+		this.nmp = nmp;
+	}
+	public String getJurusan() {
+		return jurusan;
+	}
+	public void setJurusan(String jurusan) {
+		this.jurusan = jurusan;
+	}
+	
+
+}
+
