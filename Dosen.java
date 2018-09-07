eaa6acb6bb206281424c4f642a8feb0f4d74dd22
redhita.putri <redhita.putri>
2018-09-07 14:36:23 +0700

    add file Dosen.java

---
+++ Dosen.java
@@ -0,0 +1,25 @@
+public class Dosen {
+	public String nama;
+	public String nomorDosen;
+	public String mataKuliah;
+	public String getNama() {
+		return nama;
+	}
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+	public String getNomorDosen() {
+		return nomorDosen;
+	}
+	public void setNomorDosen(String nomorDosen) {
+		this.nomorDosen = nomorDosen;
+	}
+	public String getMataKuliah() {
+		return mataKuliah;
+	}
+	public void setMataKuliah(String mataKuliah) {
+		this.mataKuliah = mataKuliah;
+	}
+	
+
+}
